ee8c20a595c691443dc783ea6beb9ede318162e1
NikitaKS <pancxxx2@gmail.com>
2019-11-14 20:09:43 +0100

    03: ToDoList my

--- src/App.js
+++ src/App.js
@@ -7,49 +7,62 @@ import TodoListFooter from "./TodoListFooter";
 class App extends React.Component {
     constructor(props){
         super(props);
-        this.newTaskTitleRef = React.createRef();
+        this.RefInput = React.createRef();
         // setTimeout(()=>{
-        //     let newTask = {text: '111', Done: false, priority: 'low'};
-        //     let newtask = [...this.state.t, newTask];
+        //     let newTask= {text: 'oo12o', Done: false, priority: 'high'};
+        //     let newtask = [...this.state.t,newTask];
         //     this.setState({
         //         t:newtask
-        //     })
+        //     });
+        // },2000)
+        // let newTask = {text: 'aaa', Done: true, priority: 'low'};
+        // let newtask = [...this.state.t, newTask];
+        // this.setState({
+        //    t:newtask
+        // });
+
     }
 
     state = {
         t : [
             {text: 'aaa', Done: true, priority: 'low'},
-            {text: 'bbb', Done: true, priority: 'medium'},
-            {text: 'ccc', Done: false, priority: 'high'},
+            {text: 'bb', Done: true, priority: 'medium'},
+            {text: 'cc', Done: false, priority: 'high'},
             {text: 'fff', Done: false, priority: 'low'},
-            {text: 'ggg', Done: true, priority: 'low'},
+            {text: 'ooo', Done: false, priority: 'high'},
         ],
-        filter : 'all'
+        filter : 'all',
     };
-     onAddTaskClick = ()=>{
-         // let newText = this.newTaskTitleRef.current.value;
-         let newTask = {text: this.newTaskTitleRef.current.value, Done: false, priority: 'low'};
-         this.newTaskTitleRef.current.value = '';
-         let newtask = [...this.state.t, newTask];
-         this.setState({
-             t:newtask
-         });
-     };
+    onAddTask = () =>{
+        this.RefInput.current.value = this.RefInput.current.value.replace(/^\s+|\s+$/g, '');
+        if(this.RefInput.current.value !== ''){
+            let newText= this.RefInput.current.value;
+            let newTask = {text: newText, Done: true, priority: 'low'};
+            this.RefInput.current.value = '';
+            let newtask = [...this.state.t, newTask];
+            this.setState({
+                t:newtask
+            });
+        } else this.RefInput.current.value = alert('Enter task name');
+        this.RefInput.current.value = '';
+
+    };
 
     render = () => {
+
         return (
             <div className="App">
                 <div className="todoList">
                     <div className="todoList-header">
                         <h3 className="todoList-header__title">What to Learn</h3>
                         <div className="todoList-newTaskForm">
-                            <input ref={this.newTaskTitleRef} type="text" placeholder="New task name"/>
-                            <button onClick={this.onAddTaskClick}>Add</button>
+                            <input ref={this.RefInput} type="text" placeholder="New task name"/>
+                            <button onClick={this.onAddTask}>Add</button>
                         </div>
                     </div>
                     {/*<TodoListHeader/>*/}
                     <TodoListTasks tt={this.state.t}/>
-                    <TodoListFooter filter={this.state.task}/>
+                    <TodoListFooter filter={this.state.filter}/>
                 </div>
             </div>
         );
